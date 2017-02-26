175900f86d49f0be3ed4db0cc96a3b15c2fd41fe
Callum Boyd <Callum Boyd>
2017-02-26 21:15:30 +0000

    Created static/media folders, started base template

---
+++ fact_or_fiction/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from factorfiction import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^factorfiction/', include('factorfiction.urls')),
+    url(r'^admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
