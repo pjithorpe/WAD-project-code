481a95ce0ca3ded5f31ddfccfeb84b9579de4636
Connor Bazely <2199728B@student.gla.ac.uk>
2017-03-10 09:12:53 +0000

    added some Admin functionality and basic population script

--- factorfiction/models.py
+++ factorfiction/models.py
@@ -1,3 +1,41 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=128)
+    postedBy = models.CharField(max_length=128,default="admin")
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    facts = models.IntegerField(default=0)
+    fictions = models.IntegerField(default=0)
+
+    slug = models.SlugField(unique=True)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Page, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'pages'
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    name = models.CharField(max_length=128)
+    age = models.IntegerField(default=0)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __str__(self):
+        return self.user.username
+    def __unicode__(self):
+        return self.title
