3908a17972c9ea20a28281b53779eef6751a6f63
Patrick Thorpe <2130613t@student.gla.ac.uk>
2017-03-24 13:20:09 +0000

    Altered dial to be more prettier.

--- static/js/loadDial.js
+++ static/js/loadDial.js
@@ -9,21 +9,48 @@ document.addEventListener('DOMContentLoaded',buildDial,false);
 		context.beginPath();
 		context.arc(125, 145, 100, 0, Math.PI, true);
 		context.closePath();
-		context.lineWidth = 5;
-		context.fillStyle = 'blue';
-		context.fill();
+		context.lineWidth = 8;
 		context.strokeStyle = '#550000';
 		context.stroke();
 		
-		var innerAngle = fictions/parseFloat(totalVotes);
+		if (totalVotes > 0) {
+			context.beginPath();
+			context.moveTo(125, 145);
+			context.arc(125, 145, 100, 0, Math.PI, true);
+			context.closePath();
+			context.fillStyle = 'blue';
+			context.fill();
+			
+			var innerAngle = -(Math.PI * (fictions/parseFloat(totalVotes)));
+			
+			context.beginPath();
+			context.moveTo(125, 145);
+			context.arc(125, 145, 100, 0, innerAngle, true);
+			context.closePath();
+			context.lineWidth = 1;
+			context.fillStyle = 'red';
+			context.fill();
+			
+			context.beginPath();
+			context.moveTo(125, 145);
+			if (innerAngle <= (Math.PI/2)){
+				context.lineTo((125 + (75*Math.cos(innerAngle))) , (145 + (75 * Math.sin(innerAngle))));
+			}
+			else{
+				context.lineTo((125 - (75*Math.cos(Math.PI - innerAngle))) , (145 - (75 * Math.sin(Math.PI - innerAngle))));
+			}
+			context.closePath();
+			context.lineWidth = 8;
+			context.strokeStyle = '#550000';
+			context.stroke();
+		}
 		
 		context.beginPath();
 		context.moveTo(125, 145);
-		context.arc(125, 145, 100, 0, -(Math.PI * innerAngle), true);
+		context.arc(125, 145, 5, 0, Math.PI, true);
 		context.closePath();
-		context.lineWidth = 5;
-		context.fillStyle = 'red';
-		context.fill();
+		context.lineWidth = 8;
 		context.strokeStyle = '#550000';
 		context.stroke();
+			
 	}
